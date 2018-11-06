ba778dd3d5e9cd841a7bb8983fb7dfb7ba21f7c3
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-06 20:57:13 -0200

    changing the parameters from the CRUD methods

--- scripts_php/clienteCRUD.php
+++ scripts_php/clienteCRUD.php
@@ -6,12 +6,15 @@
       $this->pdo = $conexao;
     }
 
-    public function CreateUser($nome,$dataNascimento,$sexo,$email,$celular,$senha,$lembreteSenha){
+    #Criando objeto da classe cliente
+    $client = new Cliente();
+
+    /*public function CreateUser($nome,$dataNascimento,$sexo,$email,$celular,$senha,$lembreteSenha){
       try{
         $sql = "INSERT into cliente (nome_cliente,data_nascimento,sexo,email,celular,senha,lembrete_senha) values (?,?,?,?,?,?,?)";
 
         $stm = $this->pdo->prepare($sql);
-        $stm->bindValue(1,$nome);
+        $stm->bindValue(1, $client$nome);
         $stm->bindValue(2,$dataNascimento);
         $stm->bindValue(3,$sexo);
         $stm->bindValue(4,$email);
@@ -23,6 +26,26 @@
 
       catch(PDOException $erro){
         echo "Erro na linha: " . $erro->getLine();
+      }
+    }*/
+
+    public function CreateUser($client){
+      try{
+        $sql = "INSERT INTO cliente (nome_cliente,data_nascimento,sexo,email,celular,senha,lembrete_senha) VALUES (?,?,?,?,?,?,?)";
+
+        $stm = $this->pdo->prepare($sql);
+        $stm->bindValue(1, $client->getNome());
+        $stm->bindValue(2, $client->getDataNascimento());
+        $stm->bindValue(3, $client->getGenero());
+        $stm->bindValue(4, $client->getEmail());
+        $stm->bindValue(5, $client->getCelular());
+        $stm->bindValue(6, $client->getSenha());
+        $stm->bindValue(7, $client->getLembreteSenha());
+        $stm->execute();
+      }
+
+      catch(PDOException $err){
+        echo "Erro na linha: " . $err->getLine();
       }
     }
 
@@ -63,18 +86,40 @@
       }
     }
 
-    public Change($nome,$dataNascimento,$genero,$email,$celular,$senha,$lembreteSenha,$code){
+    /*public Change($nome,$dataNascimento,$genero,$email,$celular,$senha,$lembreteSenha,$code){
       $sql = "update cliente set nome_cliente=?,data_nascimento=?,genero=?,email=?,celular=?,senha=?,lembreteSenha,? where id_aluno=?";
       try{
         $stmt = $this->pdo->prepare($sql);
 
-        $stmt->bindValue(1, $nome);
+        $stmt->bindValue(1, $client->getNome());
         $stmt->bindValue(2, $dataNascimento);
         $stmt->bindValue(3, $genero);
         $stmt->bindValue(4, $email);
         $stmt->bindValue(5, $celular);
         $stmt->bindValue(6, $senha);
         $stmt->bindValue(7, $lembreteSenha);
+        $stmt->bindValue(8, $code);
+
+        $stmt->execute();
+      }
+
+      catch(PDOException $err){
+        echo "Erro na linha ".$err->getLine();
+      }
+    }*/
+
+    public Change($client,$code){
+      $sql = "update cliente set nome_cliente=?,data_nascimento=?,genero=?,email=?,celular=?,senha=?,lembreteSenha,? where id_aluno=?";
+      try{
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(1, $client->getNome());
+        $stmt->bindValue(2, $client->getDataNascimento());
+        $stmt->bindValue(3, $client->getGenero());
+        $stmt->bindValue(4, $client->getEmail());
+        $stmt->bindValue(5, $client->getCelular());
+        $stmt->bindValue(6, $client->getSenha());
+        $stmt->bindValue(7, $client->getLembreteSenha());
         $stmt->bindValue(8, $code);
 
         $stmt->execute();
