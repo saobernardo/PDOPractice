fc50ba219a62bd751afbf024a54642b90ea7fb17
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-06 17:22:27 -0200

    Changing the name of attribute Sexo and its getters and setters to Genero

--- scripts_php/cadastro.process.php
+++ scripts_php/cadastro.process.php
@@ -8,7 +8,7 @@
   #Atributo
   private $nome;
   private $dataNascimento;
-  private $sexo;
+  private $genero;
   private $email;
   private $celular;
   private $senha;
@@ -20,7 +20,7 @@
   }
 
   public function getNome(){
-    return $this->nome;
+    return $nome;
   }
 
   public function setDataNascimento($dataNascimento){
@@ -28,15 +28,15 @@
   }
 
   public function getDataNascimento(){
-    return $this->dataNascimento;
+    return $dataNascimento;
   }
 
-  public function setSexo($sexo){
-    $this->sexo = $sexo;
+  public function setGenero($genero){
+    $this->genero = $genero;
   }
 
-  public function getSexo(){
-    return $this->sexo;
+  public function getGenero(){
+    return $genero;
   }
 
   public function setEmail($email){
@@ -44,7 +44,7 @@
   }
 
   public function getEmail(){
-    return $this->email;
+    return $email;
   }
 
   public function setCelular($celular){
@@ -52,15 +52,15 @@
   }
 
   public function getCelular(){
-    return $this->celular;
+    return $celular;
   }
 
   public function setSenha($senha){
-    $this->senha =$senha;
+    $this->senha = $senha;
   }
 
   public function getSenha(){
-    return $this->senha;
+    return $senha;
   }
 
   public function setLembreteSenha($lembreteSenha){
@@ -68,17 +68,18 @@
   }
 
   public function getLembreteSenha(){
-    return $this->lembreteSenha;
+    return $lembreteSenha;
   }
 
   #Pegando os nomes colocados nas caixas de texto e coloando-os nos métodos sets
+  #Nomes precisam ser inseridos nas caixas de texto
   $this->setNome($_POST['']);
   $this->setDataNascimento($_POST['']);
-  $this->setSexo($_POST['']);
+  $this->setGenero($_POST['']);
   $this->setEmail($_POST['']);
   $this->setCelular($_POST['']);
   $this->setSenha($_POST['']);
   $this->setLembreteSenha($_POST['']);
 
-  $crudCliente->CriarUsuario(getNome(),getDataNascimento(),getSexo(),getEmail(),getCelular(),getSenha(),getLembreteSenha());
+  $crudCliente->CriarUsuario(getNome(),getDataNascimento(),getGenero(),getEmail(),getCelular(),getSenha(),getLembreteSenha());
  ?>
