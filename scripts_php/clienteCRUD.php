b635f84ff0d4b390d14703e1798fc2f985a6ca7f
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-07 13:57:11 -0200

    Fixing the database code in sql local variables

--- scripts_php/clienteCRUD.php
+++ scripts_php/clienteCRUD.php
@@ -9,27 +9,7 @@
     #Criando objeto da classe cliente
     $client = new Cliente();
 
-    /*public function CreateUser($nome,$dataNascimento,$sexo,$email,$celular,$senha,$lembreteSenha){
-      try{
-        $sql = "INSERT into cliente (nome_cliente,data_nascimento,sexo,email,celular,senha,lembrete_senha) values (?,?,?,?,?,?,?)";
-
-        $stm = $this->pdo->prepare($sql);
-        $stm->bindValue(1, $client$nome);
-        $stm->bindValue(2,$dataNascimento);
-        $stm->bindValue(3,$sexo);
-        $stm->bindValue(4,$email);
-        $stm->bindValue(5,$celular);
-        $stm->bindValue(6,$senha);
-        $stm->bindValue(7,$lembreteSenha);
-        $stm->execute();
-      }
-
-      catch(PDOException $erro){
-        echo "Erro na linha: " . $erro->getLine();
-      }
-    }*/
-
-    public function CreateUser($client){
+    public function CreateUser($nome,$dataNascimento,$sexo,$email,$celular,$senha,$lembreteSenha){
       try{
         $sql = "INSERT INTO cliente (nome_cliente,data_nascimento,sexo,email,celular,senha,lembrete_senha) VALUES (?,?,?,?,?,?,?)";
 
@@ -86,7 +66,7 @@
       }
     }
 
-    /*public Change($nome,$dataNascimento,$genero,$email,$celular,$senha,$lembreteSenha,$code){
+    public Change($nome,$dataNascimento,$genero,$email,$celular,$senha,$lembreteSenha,$code){
       $sql = "update cliente set nome_cliente=?,data_nascimento=?,genero=?,email=?,celular=?,senha=?,lembreteSenha,? where id_aluno=?";
       try{
         $stmt = $this->pdo->prepare($sql);
@@ -106,30 +86,9 @@
       catch(PDOException $err){
         echo "Erro na linha ".$err->getLine();
       }
-    }*/
-
-    public Change($client,$code){
-      $sql = "update cliente set nome_cliente=?,data_nascimento=?,genero=?,email=?,celular=?,senha=?,lembreteSenha,? where id_aluno=?";
-      try{
-        $stmt = $this->pdo->prepare($sql);
-
-        $stmt->bindValue(1, $client->getNome());
-        $stmt->bindValue(2, $client->getDataNascimento());
-        $stmt->bindValue(3, $client->getGenero());
-        $stmt->bindValue(4, $client->getEmail());
-        $stmt->bindValue(5, $client->getCelular());
-        $stmt->bindValue(6, $client->getSenha());
-        $stmt->bindValue(7, $client->getLembreteSenha());
-        $stmt->bindValue(8, $code);
-
-        $stmt->execute();
-      }
-
-      catch(PDOException $err){
-        echo "Erro na linha ".$err->getLine();
-      }
     }
 
+    #Função de apagar do banco
     public function Delete($code){
       $sql = "delete from cliente where id_cliente=?";
 
