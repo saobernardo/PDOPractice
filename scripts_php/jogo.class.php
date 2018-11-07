1ff2eb8e9b5cd7e0b2dd495254554f33532451f8
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-07 20:39:16 -0200

    Created the jogo class and added the attributes

---
+++ scripts_php/jogo.class.php
@@ -0,0 +1,27 @@
+<?php
+
+  class Jogo
+  {
+     #Método construtor
+    function __construct(argument)
+    {
+
+    }
+
+    private $nome;
+    private $descricao;
+    private $dataLancamento;
+    private $precoAluguel;
+    private $faixaEtaria;
+    private $categoria;
+    private $plataforma;
+    private $tag
+
+    #função para fazer a média de notas recebidas pelo jogo
+    function Media(){
+
+    }
+
+  }
+
+ ?>
