4a938fbb9b7b659b667cef186ebc08f9b86db12e
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-14 14:39:11 -0200

    Adding the insert methods for database

--- scripts_php/jogoCRUD.class.php
+++ scripts_php/jogoCRUD.class.php
@@ -7,6 +7,21 @@
       $this->pdo = $conexao;
     }
 
-    
+    #Funções de adicionar ao banco de dados, feitas separadamente pois cada uma é em uma tabela
+    function AddGame($nome,$descricao,$dataLancamento){
+
+    }
+
+    function AddTag($precoAluguel,$tag){
+
+    }
+
+    function addFE($faixaEtaria){
+
+    }
+
+    function addCategoria($categoria){
+
+    }
   }
  ?>
