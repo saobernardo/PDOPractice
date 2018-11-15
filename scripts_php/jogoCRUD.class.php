7d4d85dbfdf68fb7f20bb63336d899369e64fdb4
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-14 22:30:22 -0200

    Adding the function lastInsertID, inserting it in a attribute and usint it int the oder Create methods

--- scripts_php/jogoCRUD.class.php
+++ scripts_php/jogoCRUD.class.php
@@ -2,6 +2,7 @@
   class jogoCRUD{
 
     private $pdo = null;
+    private $lastID = 0;
 
     function __construct($conexao){
       $this->pdo = $conexao;
@@ -10,27 +11,28 @@
     #Funções de adicionar ao banco de dados, feitas separadamente pois cada uma é em uma tabela
     function AddGame($nome,$descricao,$dataLancamento){
       try{
-        $sqlGame = "INSERT INTO jogo ($nome,$descricao,$dataLancamento) values (?,?,?)";
+        $sqlGame = "INSERT INTO jogo ($nome,$descricao,$dataLancamento) values (?,?,?) /* RETURNING id_jogo*/";
         $stmt = $this->pdo->prepare($sqlGame);
         $stmt->bindValue(1, $nome);
         $stmt->bindValue(2, $descricao);
         $stmt->bindValue(3, $dataLancamento);
         $stmt->execute();
-        /*try{
-
-        }*/
+        $this->$lastID = PDO::lastInsertID();
+        #$this->lastID = PDO::fetchAll(PDO::FETCH_ASSOC);
       }
       catch(PDOException $err){
         echo "Erro na linha: ". $err->getLine();
       }
     }
 
+
     function AddTag($tag,$precoAluguel){
       try{
-        $sqlTag = "INSERT INTO tag_jogo (nome_tag, preco_aluguel) VALUES (?,?)";
+        $sqlTag = "INSERT INTO tag_jogo (id_tag, nome_tag, preco_aluguel) VALUES (?,?,?)";
         $stmt = $this->pdo->prepare($sqlTag);
-        $stmt->bindValue(1, $tag);
-        $stmt->bindValue(2, $precoAluguel);
+        $stmt->bindValue(1, $this->lastID);
+        $stmt->bindValue(2, $tag);
+        $stmt->bindValue(3, $precoAluguel);
         $stmt->execute();
       }
       catch(PDOException $err){
@@ -40,9 +42,10 @@
 
     function addFE($faixaEtaria){
       try{
-        $sqlFaixaEtaria = "INSERT INTO faixa_etaria (faixa_etaria) VALUES (?)";
+        $sqlFaixaEtaria = "INSERT INTO faixa_etaria (id_faixaetaria, faixa_etaria) VALUES (?,?)";
         $stmt= $this->pdo->prepare($sqlFaixaEtaria);
-        $stmt->bindValue(1, $faixaEtaria);
+        $stmt->bindValue(1, $this->lastID);
+        $stmt->bindValue(2, $faixaEtaria);
         $stmt->execute();
       }
       catch(PDOException $err){
@@ -52,9 +55,10 @@
 
     function addCategoria($categoria){
       try{
-        $sqlCategoria = "INSERT INTO categoria (nome_categoria) VALUES (?)";
+        $sqlCategoria = "INSERT INTO categoria (id_categoria, nome_categoria) VALUES (?,?)";
         $stmt = $this->pdo->prepare($sqlCategoria);
-        $stmt->bindValue(1, $categoria);
+        $stmt->bindValue(1, $this->lastID);
+        $stmt->bindValue(2, $categoria);
         $stmt->execute();
       }
       catch(PDOException $err){
@@ -65,13 +69,12 @@
     #Criando método para chamar informações de um jogo
     function selectJogo($codigo){
       try {
-        
+
       }
       catch (PDOException $err) {
         echo "Erro na linha: ". $err->getLine();
       }
 
     }
-
   }
  ?>
