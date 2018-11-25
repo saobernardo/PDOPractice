9fe50cba2f417580e5ffa4971935ef7bc0f6032f
SaoBernardo <bernardo.pinheiro@hotmail.com>
2018-11-25 15:43:57 -0200

    Adding some container to define the design pages

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <title>Bem-vindo à Classy Scrub™ Renting</title>
   </head>
   <body>
-
+      <div clss="container">
+        <div class="cabecalho">
+        <div class="sign">
+          <a class="account" href="#">Entrar</a> | <a class="account" href="cadastroCliente.page.php">Cadastrar</a>
+        </div>
+        </div>
+        <br><br>
+        <div class="menu-lateral">
+            <label class="menu-lateral-link"><a class="menu-lateral-    link">Lançamentos</a><br></label>
+            <label class="menu-lateral-link"><a class="menu-lateral-    link">Clássicos</a><br></label>
+            <label class="menu-lateral-link"><a class="menu-lateral-    link">Indie</a><br></label>
+            <label class="menu-lateral-link"><a class="menu-lateral-    link">Mais jogados</a><br></label>
+            <label class="menu-lateral-link"><a class="menu-lateral-    link">Melhores notas</a><br></label>
+        </div>
+      </div>
+      
   </body>
 </html>
